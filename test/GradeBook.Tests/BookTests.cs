4ba99eca91f11bbd2735288b4ce06b86ebaa1dc4
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-10-20 14:30:09 -0500

    basic switch statement for gradebook

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Gradebook;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void BookCalculatesAnAverageGrade()
+        {
+            //arrange section
+            //var x = 5;
+            //var y = 2;
+            //var expected = 7;
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+
+            // act section
+            //var actual = x + y;
+            var result = book.GetStatistics();
+
+            //Assert section
+            //Assert.Equal(expected, actual);
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
+        }
+    }
+}
